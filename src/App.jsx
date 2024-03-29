f0e46f4edba7b3a3b53f712215710f4c12ca49a5
Rashidjuma198 <rashidjumaoti@gmail.com>
2024-03-30 00:58:44 +0300

    My A.I article summerizer

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./component/Hero";
+import Demo from "./component/Demo";
+import "./App.css";
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
