f721002bb3d8d647c974ddaca75b0a43448f4803
Rashidjuma198 <rashidjumaoti@gmail.com>
2024-03-30 12:54:39 +0300

    Addition of two components files Hero and Demonstration files

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
 const Demo = () => {
-  return <div>Demo Component</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const handlesubmit = async () => {
+    alert("Sbmitted");
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/*search*/}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className=" relative flex justify-center items-center"
+          onSubmit={handlesubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="linkIcon"
+            className="absolute left-0 my-1 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+          <button
+            className="submit_btn
+           peer-focus:border-gray-700
+          peer-focus:text-gray-700"
+          >
+            Send
+          </button>
+        </form>
+        {/* {Browse URL History} */}
+      </div>
+      {/* Display results */}
+    </section>
+  );
 };
 
 export default Demo;
